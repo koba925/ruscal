35b1c96537fbb46b54b6ddf99916ee0743607864
Kobayashi Takahiro <koba925@gmail.com>
2024-12-21 06:41:38 +0900

    構文へのマッチを実装する

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,45 @@
 fn main() {
-    println!("Hello, world!");
+    let source = "123 world";
+    println!(
+        "source: {}, parsed: {:?}",
+        source,
+        ident(whitespace(number(source)))
+    );
+}
+
+fn whitespace(mut input: &str) -> &str {
+    while matches!(input.chars().next(), Some(' ')) {
+        let mut chars = input.chars();
+        chars.next();
+        input = chars.as_str();
+    }
+    input
+}
+
+fn number(mut input: &str) -> &str {
+    if matches!(
+        input.chars().next(),
+        Some(_x @ ('-' | '+' | '.' | '0'..='9'))
+    ) {
+        while matches!(input.chars().next(), Some(_x @ ('.' | '0'..='9'))) {
+            let mut chars = input.chars();
+            chars.next();
+            input = chars.as_str();
+        }
+    }
+    input
+}
+
+fn ident(mut input: &str) -> &str {
+    if matches!(input.chars().next(), Some(_x @ ('a'..='z' | 'A'..='Z'))) {
+        while matches!(
+            input.chars().next(),
+            Some(_x @ ('a'..='z' | 'A'..='Z' | '0'..='9'))
+        ) {
+            let mut chars = input.chars();
+            chars.next();
+            input = chars.as_str();
+        }
+    }
+    input
 }
